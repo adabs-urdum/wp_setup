b93deccd07c0943bd39d9594323e68b52840ecbe
cyrill.lehmann <cyrill@adabs.ch>
2018-11-29 15:48:36 +0100

    added three js setup and img, canvas as display:block

--- wp-content/themes/wp-setup/assets/js/main.js
+++ wp-content/themes/wp-setup/assets/js/main.js
@@ -17,4 +17,53 @@
     init();
   };
 
+  function WebGLThreeJS(){
+    var scene,
+        camera,
+        renderer;
+
+    function init(){
+      setVars();
+      bindEvents();
+      initThree();
+      mainLoop();
+    }
+
+    function setVars(){
+    }
+
+    function bindEvents(){
+    }
+
+    function initThree(){
+      createScene();
+      createRenderer();
+      createPerspectiveCamera();
+    }
+
+    function createScene(){
+      scene = new THREE.Scene();
+    }
+
+    function createRenderer(){
+      renderer = new THREE.WebGLRenderer();
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      document.body.appendChild(renderer.domElement);
+    }
+
+    function createPerspectiveCamera(){
+      camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 1, 501);
+      camera.position.z = 10;
+      camera.position.x = 0;
+      camera.position.y = 0;
+    }
+
+    function mainLoop(){
+      renderer.render(scene, camera);
+      requestAnimationFrame(mainLoop);
+    }
+
+    init();
+  }
+
 })(jQuery);
