2c1207f1d824002cdad8789eb01145e7cce5b8c9
cyrill.lehmann <cyrill@adabs.ch>
2018-12-10 09:06:33 +0100

    added image sizes and download plugins without providing ftp stuff

--- wp-content/themes/wp-setup/functions.php
+++ wp-content/themes/wp-setup/functions.php
@@ -216,4 +216,20 @@ function disable_emojis_remove_dns_prefetch($urls, $relation_type) {
   return $urls;
 }
 
+//Download Plugins without providing FTP Info
+define('FS_METHOD', 'direct');
+
+// Available Image Sizes – Ratio 4:3
+// ----------------------------------------------------------
+add_image_size( 'XL', 2000, 1500, true );
+add_image_size( 'XL_portrait', 1500, 2000, true );
+add_image_size( 'L', 1450, 1087, true );
+add_image_size( 'L_portrait', 1087, 1450, true );
+add_image_size( 'M', 1024, 768, true );
+add_image_size( 'M_portrait', 768, 1024, true );
+add_image_size( 'S', 768, 576, true );
+add_image_size( 'S_portrait', 576, 768, true );
+add_image_size( 'XS', 512, 384, true );
+add_image_size( 'XS_portrait', 384, 512, true );
+
 ?>
